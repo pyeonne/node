033da872441c782acc88be6aa33366948be5f00e
pyeonne <pyeonne@gmail.com>
2022-03-03 00:14:54 +0900

    [🔨 Refactor]: 서버 Data Repository 구현 (#4)

---
+++ Backend A-Z/dwitter/server/data/tweet.js
@@ -0,0 +1,54 @@
+let tweets = [
+    {
+        id: '1',
+        text: '프론트엔드 개발자 취뽀하자!',
+        createdAt: Date.now().toString(),
+        name: 'Bob',
+        username: 'bob',
+        url: 'https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg',
+    },
+    {
+        id: '2',
+        text: '프론트엔드 개발자 지망생',
+        createdAt: Date.now().toString(),
+        name: 'Pyeonne',
+        username: 'pyeonne',
+    },
+];
+
+export function getAll() {
+    return tweets;
+}
+
+export function getAllByUsername(username) {
+    return tweets.filter(tweet => tweet.username === username);
+}
+
+export function getById(id) {
+    return tweets.find(tweet => tweet.id === id);
+}
+
+export function create(text, name, username) {
+    const tweet = {
+        id: Date.now().toString(),
+        text,
+        createdAt: new Date(),
+        name,
+        username,
+    };
+    tweets = [tweet, ...tweets];
+    return tweet;
+}
+
+export function update(id, text) {
+    const tweet = tweets.find(tweet => tweet.id === id);
+    if (tweet) {
+        tweet.text = text;
+    }
+
+    return tweet;
+}
+
+export function remove(id) {
+    tweets = tweets.filter(tweet => tweet.id !== id);
+}
