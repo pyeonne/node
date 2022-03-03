11eef2206a0810d5a3612e4ba72656bb275bb3a9
pyeonne <pyeonne@gmail.com>
2022-03-03 14:18:27 +0900

    [🔨 Refactor]: Http Client 구현 (#5)

--- Backend A-Z/dwitter/client/src/index.js
+++ Backend A-Z/dwitter/client/src/index.js
@@ -7,22 +7,24 @@ import TweetService from './service/tweet';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AuthErrorEventBus } from './context/AuthContext';
+import HttpClient from './network/http';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
+const httpClient = new HttpClient(baseURL);
 const authErrorEventBus = new AuthErrorEventBus();
 const authService = new AuthService();
-const tweetService = new TweetService(baseURL);
+const tweetService = new TweetService(httpClient);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider
-        authService={authService}
-        authErrorEventBus={authErrorEventBus}
-      >
-        <App tweetService={tweetService} />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <AuthProvider
+                authService={authService}
+                authErrorEventBus={authErrorEventBus}
+            >
+                <App tweetService={tweetService} />
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
