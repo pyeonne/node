c295930b6551d42fa74bd0d1f79c8db6e40858f7
pyeonne <pyeonne@gmail.com>
2022-03-03 16:58:37 +0900

    [📚 Docs]: Validation 노트 작성

---
+++ Backend A-Z/14-validation/app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import { body, param, validationResult } from 'express-validator';
+
+const app = express();
+app.use(express.json());
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        return next();
+    }
+    return res.status(400).json({ message: errors.array()[0].msg });
+};
+
+/**
+ * trim() - 공백 제거
+ * normalizeEmail() - 이메일 소문자로 변경
+ */
+app.post(
+    '/users',
+    [
+        body('name')
+            .trim()
+            .isLength({ min: 2 })
+            .withMessage('이름은 두글자 이상!'),
+        body('age').isInt().withMessage('숫자를 입력해'),
+        body('email').isEmail().withMessage('이메일 입력해요').normalizeEmail(),
+        body('job.name').notEmpty(),
+        validate,
+    ],
+    (req, res, next) => {
+        console.log(req.body);
+        res.sendStatus(201);
+    },
+);
+
+app.get(
+    '/:email',
+    [param('email').isEmail().withMessage('이메일 입력해요'), validate],
+    (req, res, next) => {
+        res.send('💌');
+    },
+);
+
+app.listen(8080);
