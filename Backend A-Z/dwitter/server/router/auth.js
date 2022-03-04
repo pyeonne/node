12d3e22b58c446f5d832a207aab347f92306d7ed
pyeonne <pyeonne@gmail.com>
2022-03-04 10:53:41 +0900

    [🎨 Feat]: Auth 미들웨어 구현 (#7)

--- Backend A-Z/dwitter/server/router/auth.js
+++ Backend A-Z/dwitter/server/router/auth.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { body } from 'express-validator';
 import { validate } from '../middleware/validator.js';
 import * as authController from '../controller/auth.js';
+import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
@@ -31,5 +32,7 @@ const validateSignup = [
 router.post('/signup', validateSignup, authController.signup);
 
 router.post('/login', validateCredential, authController.login);
+
+router.get('/me', isAuth, authController.me);
 
 export default router;
--- Backend A-Z/dwitter/server/router/tweets.js
+++ Backend A-Z/dwitter/server/router/tweets.js
@@ -2,8 +2,12 @@ import express from 'express';
 import { body } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
 import { validate } from '../middleware/validator.js';
+import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
+
+// validation - 유효성 검사
+// sanitization - 일관성
 
 const validateTweet = [
     body('text')
@@ -15,18 +19,18 @@ const validateTweet = [
 
 // GET /tweets
 // GET /tweets?username=:username
-router.get('/', tweetController.getTweets);
+router.get('/', isAuth, tweetController.getTweets);
 
 // GET /tweets/:id
-router.get('/:id', tweetController.getTweet);
+router.get('/:id', isAuth, tweetController.getTweet);
 
 // POST /tweets
-router.post('/', validateTweet, tweetController.createTweet);
+router.post('/', isAuth, validateTweet, tweetController.createTweet);
 
 // PUT /tweets:id
-router.put('/:id', validateTweet, tweetController.updateTweet);
+router.put('/:id', isAuth, validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets:id
-router.delete('/:id', tweetController.deleteTweet);
+router.delete('/:id', isAuth, tweetController.deleteTweet);
 
 export default router;
