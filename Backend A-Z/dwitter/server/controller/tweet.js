8586792683b34d4266b75e1abeadc750dff96f79
pyeonne <pyeonne@gmail.com>
2022-03-07 13:22:31 +0900

    [🎨 Feat]: Authorization 권한 부여 (#7)

--- Backend A-Z/dwitter/server/controller/tweet.js
+++ Backend A-Z/dwitter/server/controller/tweet.js
@@ -27,16 +27,28 @@ export async function createTweet(req, res) {
 export async function updateTweet(req, res) {
     const id = req.params.id;
     const text = req.body.text;
-    const tweet = await tweetRepository.update(id, text);
-    if (tweet) {
-        res.status(200).json(tweet);
-    } else {
-        res.status(404).json({ message: `Tweet id(${id}) not found` });
+    const tweet = await tweetRepository.getById(id);
+    if (!tweet) {
+        return res.sendStatus(404);
     }
+    // 401은 로그인이 필요한 서비스인데 로그인을 하지 않았을 때
+    // 403 로그인된 사용자지만 특별한 권한이 없을 때
+    if (tweet.userId !== req.userId) {
+        return res.sendStatus(403);
+    }
+    const updated = await tweetRepository.update(id, text);
+    res.status(200).json(updated);
 }
 
 export async function deleteTweet(req, res) {
     const id = req.params.id;
+    const tweet = await tweetRepository.getById(id);
+    if (!tweet) {
+        return res.sendStatus(404);
+    }
+    if (tweet.userId !== req.userId) {
+        return res.sendStatus(403);
+    }
     await tweetRepository.remove(id);
     res.sendStatus(204);
 }
