b843969acc65957dbdaeed0d198800ee89e1931e
pyeonne <pyeonne@gmail.com>
2022-03-07 14:16:58 +0900

    [🔨 Refactor]: 재사용성 및 안전성 향상 (#8)

--- Backend A-Z/dwitter/server/app.js
+++ Backend A-Z/dwitter/server/app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import tweetsRoute from './router/tweets.js';
 import authRoute from './router/auth.js';
+import { config } from './config.js';
 
 const app = express();
 
@@ -24,4 +25,4 @@ app.use((error, req, res, next) => {
     res.sendStatus(500);
 });
 
-app.listen(8080);
+app.listen(config.host.port);
