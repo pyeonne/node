dfb2b14952a1ddd246a39fe2f2d4d7a7b13099d6
pyeonne <pyeonne@gmail.com>
2022-03-31 17:24:49 +0900

    [🎨 Feat]: 트윈 관련  Mongoose 적용 (#14)

--- Backend A-Z/dwitter/server/data/tweet.js
+++ Backend A-Z/dwitter/server/data/tweet.js
@@ -1,74 +1,47 @@
-// import MongoDb from 'mongodb';
-// import { getTweets } from '../database/database.js';
-// import * as userRepository from '../data/auth.js';
-// const ObjectId = MongoDb.ObjectId;
+import Mongoose from 'mongoose';
+import { useVirtualId } from '../database/database.js';
+import * as UserRepository from '../data/auth.js';
 
-// // NoSQL (정보의 중복 > 관계)
-// // 모든 사용자가 트윗을 쿼리하는 횟수 > 사용자가 사용자의 정보를 업데이트를 하는 횟수
-// // 관계형 조인 쿼리의 성능이 좋지 않다.
+const tweetSchema = new Mongoose.Schema(
+    {
+        text: { type: String, required: true },
+        userId: { type: String, required: true },
+        username: { type: String, required: true },
+        url: String,
+    },
+    { timestamps: true },
+);
 
-// export async function getAll() {
-//     return getTweets() //
-//         .find()
-//         .sort({ createdAt: -1 })
-//         .toArray()
-//         .then(mapTweets);
-// }
+useVirtualId(tweetSchema);
+const Tweet = Mongoose.model('Tweet', tweetSchema);
 
-// export async function getAllByUsername(username) {
-//     return getTweets() //
-//         .find({ username })
-//         .sort({ createdAt: -1 })
-//         .toArray()
-//         .then(mapTweets);
-// }
+export async function getAll() {
+    return Tweet.find().sort({ createdAt: -1 });
+}
 
-// export async function getById(id) {
-//     return getTweets() //
-//         .findOne({ _id: new ObjectId(id) })
-//         .then(mapOptionalTweet);
-// }
+export async function getAllByUsername(username) {
+    return Tweet.find({ username }).sort({ createdAt: -1 });
+}
 
-// export async function create(text, userId) {
-//     const { name, username, url } = await userRepository.findById(userId);
-//     const tweet = {
-//         text,
-//         createdAt: new Date(),
-//         userId,
-//         name,
-//         username,
-//         url,
-//     };
-//     return getTweets()
-//         .insertOne(tweet)
-//         .then(data =>
-//             mapOptionalTweet({
-//                 ...tweet,
-//                 _id: data.insertedId,
-//             }),
-//         );
-// }
+export async function getById(id) {
+    return Tweet.findById(id);
+}
 
-// export async function update(id, text) {
-//     return getTweets()
-//         .findOneAndUpdate(
-//             { _id: new ObjectId(id) },
-//             { $set: { text } },
-//             { returnDocument: 'after' },
-//         )
-//         .then(result => result.value)
-//         .then(mapOptionalTweet);
-// }
+export async function create(text, userId) {
+    return UserRepository.findById(userId).then(user =>
+        new Tweet({
+            text,
+            userId,
+            name: user.name,
+            username: user.username,
+        }).save(),
+    );
+}
 
-// export async function remove(id) {
-//     return getTweets().deleteOne({ _id: new ObjectId(id) });
-// }
+export async function update(id, text) {
+    return Tweet.findByIdAndUpdate(id, { text }, { returnOriginal: false });
+}
 
-// function mapOptionalTweet(tweet) {
-//     return tweet ? { ...tweet, id: tweet._id.toString() } : tweet;
-// }
-
-// function mapTweets(tweets) {
-//     console.log(tweets);
-//     return tweets.map(mapOptionalTweet);
-// }
+export async function remove(id) {
+    return Tweet.findByIdAndDelete(id);
+}
